61305b967007b134e27143e42fb95d90f6e7c5f5
sahildadhwal <97374232+sahildadhwal@users.noreply.github.com>
2025-02-15 20:36:34 -0800

    add criteria based on ref report

--- getRepos.py
+++ getRepos.py
@@ -28,15 +28,67 @@ GITHUB_API_URL = "https://api.github.com/search/repositories"
 
 tr = ToxicityRater()
 
+def get_count(field, owner, repo):
+    url = f"https://api.github.com/repos/{owner}/{repo}/{field}"
+    response = requests.get(url, headers=HEADERS)
+    if response.status_code != 200:
+        return 0
+    
+    # Get count of Issues
+    if field == "issues":
+        return len([issue for issue in response.json() if "pull_request" not in issue]) 
+    
+    # Get count of Contributors or Pulls
+    elif field in ["contributors", "pulls"]:
+        return len(response.json()) 
+        
+    else:
+        print("!!!TYPO in get_count(field, ....)")
 
 
-def search_repositories():
+# Filter issues to be locked and have label "too-heated", "off-topic", or "spam"  (ref: https://github.com/vcu-swim-lab/incivility-dataset)
+def get_locked_issues_by_labels(owner, repo, target_labels):
+    # Returns a list of issues which are locked and have at least one label in target_labels.
+    url = f"https://api.github.com/repos/{owner}/{repo}/issues?state=all&per_page=100" # state=all get both open and closed issues.
+    response = requests.get(url, headers=HEADERS)
+    if response.status_code != 200:
+        return []
+    
+    issues = response.json()
+    filtered_issues = []
+    for issue in issues:
+        # Check if the issue is locked.
+        if issue.get("locked", False):
+            # Check if any of its labels match our target labels.
+            for label in issue.get("labels", []):
+                if label.get("name", "").lower() in target_labels:
+                    filtered_issues.append(issue)
+                    break
+    return filtered_issues
+
+
+
+
+# Given a repo, return the comments where comment_type can be "Issues" or "Pulls"
+def get_comments(owner, repo, comment_type):
+    url = f"https://api.github.com/repos/{owner}/{repo}/{comment_type}/comments"
+    response = requests.get(url, headers=HEADERS)
+    if response.status_code == 200:
+        return response.json() 
+    else: 
+        return []
+        
+
+
+def search_repositories(page, per_page):
     # TODO: Condition 1
     params = {
-        "q": "stars:>10 created:>=2020-01-01",
+        #"q": "stars:>10 created:>=2020-01-01",
+        "q": "stars:>10 created:>=2013-04-07",
         "sort": "stars",
         "order": "desc",
-        "per_page": 20 # number of repositories
+        "per_page": per_page, #20 # number of repositories
+        "page": page
     }
 
     response = requests.get(GITHUB_API_URL, params=params)
@@ -51,82 +103,29 @@ def search_repositories():
         return []
     
 
-# Given a repo, return the comments where comment_type can be "Issues" or "Pulls"
-def get_comments(owner, repo, comment_type):
-    url = f"https://api.github.com/repos/{owner}/{repo}/{comment_type}/comments"
-    response = requests.get(url, headers=HEADERS)
-    if response.status_code == 200:
-        return response.json() 
-    else: 
-        return []
-        
-
 # Given a list of comments, return their mean toxicity ranking using the Perspective API
 def analyze_toxicity(metric, comments):
-    if metric == "mean":
-        scores = []
-        for comment in comments:
-            scores.append(tr.get_toxicity_rating(comment['body'], language='en' ))
-
-            # Sleep 1s to prevent Perspective API request limit
-            time.sleep(1)
-
-        if scores:
-            return sum(scores) / len(scores) 
-        else:
-            # suspiciously low toxicity
-            print("!!!Possible Issue: 0% toxicity")
-            return 0
-    
-    else: 
+    if metric not in ["mean"]:
         print("!!!Please include a LISTED metric (ex: mean)")
         return 0
-#
-def filter_repositories(repositories):
-    filtered_repos = []
-    for repo in repositories:
-        owner, name = repo["full_name"].split("/")
-        contributors_count = get_count("contributors",owner, name)
-        pr_count = get_count("pulls", owner, name)
-        issues_count = get_count("issues",owner, name)
-        details = get_repo_details(owner, name)
-        
-        issue_comments = get_comments(owner, name, "issues")
-        pr_comments = get_comments(owner, name, "pulls")
-        
-
 
     
-        if details is None:
-            continue
+    
+    scores = []
+    for comment in comments:
+        scores.append(tr.get_toxicity_rating(comment['body'], language='en' ))
 
-        activity_duration = details["last_update"] - details["created_at"]
+        # Sleep 1s to prevent Perspective API request limit
+        time.sleep(1)
 
-        # TODO: something like if (activity_duration.days >= 90) and (activity_duration.days < 180):
+    if scores:
+        return sum(scores) / len(scores) 
+    else:
+        # suspiciously low toxicity
+        print("!!!Possible Issue: 0% toxicity")
+        return 0
 
-
-        # TODO: Condition 2
-        # if pr_count >= 10 and contributors_count >= 5 and issues_count>=10 and 90 <= activity_duration.days < 180:
-        if pr_count >= 5:
-            filtered_repos.append({
-                "repo_name": details["repo_name"],
-                "repo_url": details["repo_url"],
-                "created_at": details["created_at"],
-                "last_update": details["last_update"],
-                "pr_count": pr_count,
-                "issues_count": issues_count,
-                "contributors_count": contributors_count,
-
-                "mean_issue_toxicity": analyze_toxicity("mean",issue_comments),
-                "mean_pr_toxicity": analyze_toxicity("mean", pr_comments)
-            })
-            print(f"Matched: {details['repo_name']} (Issues: {issues_count}, Contributors: {contributors_count})")
-
-    return filtered_repos
-
-
-# For filtering repos ====================================
-
+#
 def get_repo_details(owner, repo):
     url = f"https://api.github.com/repos/{owner}/{repo}"
     response = requests.get(url, headers=HEADERS)
@@ -144,22 +143,87 @@ def get_repo_details(owner, repo):
             }
     return None
 
-def get_count(field, owner, repo):
-    url = f"https://api.github.com/repos/{owner}/{repo}/{field}"
-    response = requests.get(url, headers=HEADERS)
-    if response.status_code != 200:
-        return 0
-    
-    # Get count of Issues
-    if field == "issues":
-        return len([issue for issue in response.json() if "pull_request" not in issue]) 
-    
-    # Get count of Contributors or Pulls
-    elif field in ["contributors", "pulls"]:
-        return len(response.json()) 
+# For filtering repos ====================================
+def filter_repositories(repositories):
+    filtered_repos = []
+
+    # Set timeframe timeframe TODO: changge timeframe to present?
+    # "from April 7, 2013, to October 24, 2023"  (ref: https://github.com/vcu-swim-lab/incivility-dataset)
+    start_time , end_time = (datetime(2013, 4, 7), datetime(2023, 10, 24))
+    # Desired issues have at least 1 of the following labels "too-heated", "off-topic", or "spam" (ref: https://github.com/vcu-swim-lab/incivility-dataset)
+    target_labels = {"too-heated", "off-topic", "spam"}
+
+    for repo in repositories:
+        owner, name = repo["full_name"].split("/")
+        contributors_count = get_count("contributors",owner, name)
+        pr_count = get_count("pulls", owner, name)
+        issues_count = get_count("issues",owner, name)
+        details = get_repo_details(owner, name)
+        if details is None:
+            continue
+
+        issue_comments = get_comments(owner, name, "issues")
+        pr_comments = get_comments(owner, name, "pulls")
         
-    else:
-        print("!!!TYPO in get_count(field, ....)")
+
+
+    
+        if details is None:
+            continue
+
+        # Criteria #1 Make sure repo is within timeframe
+        # handled in "params"
+        #if (details["created_at"] < start_time) or (details["created_at"] > end_time):
+            # Skip, Out of date range 
+            #continue
+        
+        # Criteria #2 Make sure at least 50 contributors 
+        if contributors_count < 10: #50:
+            # Skip, not enought contributors
+            continue
+
+        # Criteria #3 Make sure issue is locked & has target label
+        locked_issues = get_locked_issues_by_labels(owner, name, target_labels)
+        if len(locked_issues) == 0:
+            # Skip, doesnt match critera 3
+            continue
+        # Critera #4 MORE?
+        
+
+        # activity_duration = details["last_update"] - details["created_at"]
+        # EX: if 90 <= activity_duration.days < 180:
+
+
+
+        # TODO: something like if (activity_duration.days >= 90) and (activity_duration.days < 180):
+
+
+        # TODO: Condition 2
+        # if pr_count >= 10 and contributors_count >= 5 and issues_count>=10 and 90 <= activity_duration.days < 180:
+        if pr_count >= 5:
+            filtered_repos.append({
+                "repo_name": details["repo_name"],
+                "repo_url": details["repo_url"],
+                "created_at": details["created_at"],
+                "last_update": details["last_update"],
+                "pr_count": pr_count,
+                "issues_count": issues_count,
+                "contributors_count": contributors_count,
+
+                "locked_issues_count": len(locked_issues),
+
+
+                "mean_issue_toxicity": analyze_toxicity("mean",issue_comments),
+                "mean_pr_toxicity": analyze_toxicity("mean", pr_comments)
+            })
+            #print(f"Matched: {details['repo_name']} (Issues: {issues_count}, Contributors: {contributors_count})")
+            print(f"Matched: {details['repo_name']} (Contributors: {contributors_count}, Locked Issues: {len(locked_issues)})")
+
+    return filtered_repos
+
+
+
+
 # For saving data ====================================
 
 def save_to_csv(filtered_repos):
@@ -190,9 +254,38 @@ def save_to_csv(filtered_repos):
     print(f"found {cnt} repositories -> saved to {csv_filename}")
 
 def main():
-    repositories = search_repositories()
-    filtered_repos = filter_repositories(repositories)
-    save_to_csv(filtered_repos)
+
+    target_repo_count = 20 # TODO: change here to how many repos we want 
+    all_filtered_repos = []
+    page = 1
+    # How many repos to fetch per page
+    per_page = 100 #20
+
+    while len(all_filtered_repos) < target_repo_count:
+        repositories = search_repositories(page, per_page)
+        if not repositories:
+            print("No more repositories found from the search API.")
+            break
+        
+        filtered = filter_repositories(repositories)
+        all_filtered_repos.extend(filtered)
+        print(f"Total filtered repos so far: {len(all_filtered_repos)}")
+        
+        # Increase page number for pagination
+        page += 1
+        
+        # Optional: Sleep to avoid hitting rate limits on the search API.
+        time.sleep(2)
+
+        # Slice the list to exactly the target count.
+        final_repos = all_filtered_repos[:target_repo_count]
+        save_to_csv(final_repos)
+        
+    
+    
+    #repositories = search_repositories()
+    #filtered_repos = filter_repositories(repositories)
+    #save_to_csv(filtered_repos)
 
 
 if __name__ == "__main__":

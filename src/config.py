a7025655ed4dcc36e5feb1ef8b71675dee92bf98
sahildadhwal <97374232+sahildadhwal@users.noreply.github.com>
2025-03-04 22:34:54 -0800

    get repo links directly from datasource

--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,23 +15,29 @@ analysis_window_days = 7  # Days before/after toxic comment to analyze
 release_window_days = 14  # Days before release to analyze for toxicity increase
 
 # Predefined issue URLs to analyze (from the Github DB https://github.com/vcu-swim-lab/incivility-dataset/blob/main/dataset/issue_threads.csv)
-issue_urls = [
-      "https://api.github.com/repos/doctrine/mongodb-odm/issues/554"
-    , "https://api.github.com/repos/capistrano/capistrano/issues/440"
-    , "https://api.github.com/repos/Leaflet/Leaflet/issues/2195"
-    , "https://api.github.com/repos/composer/composer/issues/2545"
+
+
+# Load the CSV file directly from the URL
+url = "https://raw.githubusercontent.com/vcu-swim-lab/incivility-dataset/main/dataset/issue_threads.csv"
+data = pd.read_csv(url)
+issue_urls = data['url'].tolist()
+
+# LIMIT HOW MANY URLS WE USE
+# WE WILL SPLIT THIS UP bc its slow
+issue_urls = issue_urls[0:2]
+
+# issue_urls = [
+#       "https://api.github.com/repos/doctrine/mongodb-odm/issues/554"
+#     , "https://api.github.com/repos/capistrano/capistrano/issues/440"
+#     , "https://api.github.com/repos/Leaflet/Leaflet/issues/2195"
+#     , "https://api.github.com/repos/composer/composer/issues/2545"
     
-    , "https://api.github.com/repos/mysqljs/mysql/issues/731"
-    , "https://api.github.com/repos/PCSX2/pcsx2/issues/10"
-    , "https://api.github.com/repos/openzfs/zfs/issues/2611"
-    , "https://api.github.com/repos/thoughtbot/factory_bot/issues/683"
-    , "https://api.github.com/repos/scikit-learn/scikit-learn/issues/3808"
     
-    # TODO: Add more issue URLs here
-    # we can each choose differnt ones to run and write to different CSVs
-    # and merge the CSVs into 1 for analysis
-    # will write a script to directly get urls from here https://github.com/vcu-swim-lab/incivility-dataset/blob/main/dataset/issue_threads.csv
-]
+#     # TODO: Add more issue URLs here
+#     # we can each choose differnt ones to run and write to different CSVs
+#     # and merge the CSVs into 1 for analysis
+#     # will write a script to directly get urls from here https://github.com/vcu-swim-lab/incivility-dataset/blob/main/dataset/issue_threads.csv
+# ]
 
 # Headers for GitHub API requests
 headers = {
